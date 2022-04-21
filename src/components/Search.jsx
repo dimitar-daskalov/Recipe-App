8821486ad63de41ffbe94ebedd5cf6937489c59e
Dimitar Daskalov <dimitar.daskalov@ad.mentormate.bg>
2022-04-21 18:27:04 +0300

    Simple Recipe app with React

---
+++ src/components/Search.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+
+const Search = () => {
+  const [input, setInput] = useState("");
+  const navigate = useNavigate();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    navigate("/searched/" + input);
+  };
+
+  return (
+    <FormStyle onSubmit={submitHandler}>
+      <div>
+        <FaSearch />
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          required
+        />
+      </div>
+    </FormStyle>
+  );
+};
+
+const FormStyle = styled.form`
+  margin: 0rem 8rem;
+
+  div {
+    position: relative;
+    width: 100%;
+  }
+
+  input {
+    border: none;
+    background: linear-gradient(35deg, #494949, #313131);
+    font-size: 1rem;
+    color: white;
+    padding: 1rem 2rem;
+    border: none;
+    border-radius: 1rem;
+    outline: none;
+    width: 100%;
+  }
+
+  svg {
+    position: absolute;
+    top: 50%;
+    left: -1%;
+    color: white;
+    transform: translate(100%, -50%);
+  }
+`;
+export default Search;
